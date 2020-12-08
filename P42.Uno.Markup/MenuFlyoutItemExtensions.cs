817ed709b3aad560894297fbda85fd8b37f23fb5
Ben Askren <ben@buildcalc.com>
2020-12-08 15:08:17 -0500

    - Update to Uno 3.3.0; - Added MenuFlyoutItem.OnClick() extension method;

---
+++ P42.Uno.Markup/MenuFlyoutItemExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using ElementType = Windows.UI.Xaml.Controls.MenuFlyoutItem;
+
+namespace P42.Uno.Markup
+{
+    public static class MenuFlyoutItemExtensions
+    {
+        public static TElement OnClick<TElement>(this TElement element, RoutedEventHandler click) where TElement : ElementType
+        {
+            element.Click += click;
+            return element;
+        }
+    }
+}
