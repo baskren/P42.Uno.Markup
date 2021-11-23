36adb39ecdf5608fccea0366304b121fdced0b34
Ben Askren <ben@buildcalc.com>
2021-11-23 11:19:00 -0500

    UPDATE (Nuget Packages) : Uno.UI to 3.11.6

---
+++ P42.Uno.Markup/ProgressRingExtensions.cs
@@ -0,0 +1,23 @@
+using P42.Utils.Uno;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace P42.Uno.Markup
+{
+    public static class ProgressRingExtensions
+    {
+        public static ProgressRing Active(this ProgressRing element, bool value = true) 
+        { element.IsActive = value; return element; }
+    }
+}
