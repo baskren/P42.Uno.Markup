af501eee1ac4ab187ff3fc5af1a03b52aa763573
baskren <ben@buildcalc.com>
2023-03-09 12:09:47 -0500

    FEAT : SystemTeachingTipBrushes

--- P42.Uno.Markup/SystemBrushes/SystemContentDialogBrushes.cs
+++ P42.Uno.Markup/SystemBrushes/SystemContentDialogBrushes.cs
@@ -11,9 +11,9 @@ namespace P42.Uno.Markup
     // Full list of system brushes found here: https://github.com/MicrosoftDocs/windows-uwp/issues/2072
     public static class SystemContentDialogBrushes
     {
-        public static Brush Background => ColorExtensions.AppBrush("ContentDialogBackgroundThemeBrush");
-        public static Brush Border => ColorExtensions.AppBrush("ContentDialogBorderThemeBrush");
-        public static Brush ContentForeg => ColorExtensions.AppBrush("ContentDialogContentForegroundBrush");
+        public static Brush Background => ColorExtensions.AppBrush("ContentDialogBackground");
+        public static Brush Border => ColorExtensions.AppBrush("ContentDialogBorderBrush");
+        public static Brush Foreground => ColorExtensions.AppBrush("ContentDialogForeground");
         public static Brush Dimming => ColorExtensions.AppBrush("ContentDialogDimmingThemeBrush");
     }
 }
---
+++ P42.Uno.Markup/SystemBrushes/SystemTeachingTipBrushes.cs
@@ -0,0 +1,18 @@
+using Microsoft.UI.Xaml.Media;
+
+namespace P42.Uno.Markup
+{
+    // XAML theme resources
+    // The XAML color ramp and theme-dependent brushes
+    // Light and Dark theme colors
+    // https://github.com/microsoft/microsoft-ui-xaml/blob/main/dev/CommonStyles/Button_themeresources.xaml
+    public static class SystemTeachingTipBrushes
+    {
+        public static Brush Background => ColorExtensions.AppBrush("TeachingTipBackgroundBrush");
+
+        public static Brush Foreground => ColorExtensions.AppBrush("TeachingTipForegroundBrush");
+
+        public static Brush Border => ColorExtensions.AppBrush("TeachingTipBorderBrush");
+
+    }
+}
